3061df575bec5d38b85625a23bd1824f83ddcef7
buzzz321 <buzzz321@github.com>
2011-11-22 19:45:52 +0100

    more algo work

--- Imagecontrol.cc
+++ Imagecontrol.cc
@@ -10,31 +10,32 @@
 #include <cassert>
 
 ImageControl::ImageControl() {
-	// TODO Auto-generated constructor stub
+    // TODO Auto-generated constructor stub
 
 }
 
 ImageControl::ImageControl(std::string filename) {
-	m_myImage.setFilename(filename);
+    m_myImage.setFilename(filename);
 }
 
-int
-ImageControl::unshredd() {
-	bool ok = m_myImage.loadPNG();
+int ImageControl::unshredd() {
+    bool ok = m_myImage.loadPNG();
 
-	assert(ok);
-	for (unsigned int shreddnr = 0; shreddnr < m_myImage.getShredds() - 1; ++shreddnr) {
-		std::cout << shreddnr <<std::endl;
+    assert(ok);
+    for (unsigned int shreddnr = 0; shreddnr < m_myImage.getShredds() - 1; ++shreddnr) {
+        std::cout << shreddnr << std::endl;
 
-		std::vector<RGBQUAD> rightborder = m_myImage.getShredPixelBorders(shreddnr, false);
-		std::vector<RGBQUAD> leftborder = m_myImage.getShredPixelBorders(shreddnr + 1, true);
+        std::vector<RGBQUAD> rightborder = m_myImage.getShredPixelBorders(shreddnr, false);
+        for (unsigned int testshredd = 1; testshredd < m_myImage.getShredds(); ++testshredd) {
+            std::vector<RGBQUAD> leftborder = m_myImage.getShredPixelBorders(testshredd, true);
+        }
 
-	}
+    }
 
-	return 0;
+    return 0;
 }
 
 ImageControl::~ImageControl() {
-	// TODO Auto-generated destructor stub
+    // TODO Auto-generated destructor stub
 }
 
