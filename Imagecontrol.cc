2963c4b3bad6f8344b890f0b79ab35efec6fba86
buzzz321 <buzzz321@github.com>
2011-11-29 21:32:13 +0100

    tired :)

--- Imagecontrol.cc
+++ Imagecontrol.cc
@@ -39,10 +39,17 @@ int ImageControl::unshredd() {
 
 int ImageControl::calc(std::vector<RGBQUAD> leftborder, std::vector<RGBQUAD> rightborder) {
     std::vector<RGBQUAD> retVal;
+    unsigned long red = 0;
+    unsigned long green = 0;
+    unsigned long blue = 0;
 
      for(size_t index = 0; index < leftborder.size(); ++index) {
          RGBQUAD p1 = leftborder[index];
          RGBQUAD p2 = rightborder[index];
+
+         red += abs(p2.rgbRed-p1.rgbRed);
+         green += abs(p2.rgbGreen-p1.rgbGreen);
+         blue += abs(p2.rgbBlue-p1.rgbBlue);
          std::cout <<(int) p2.rgbRed <<": "<< abs(p2.rgbRed-p1.rgbRed) << " " << abs(p2.rgbGreen-p1.rgbGreen) << " " << abs(p2.rgbBlue-p1.rgbBlue) << "\n";
      }
 
